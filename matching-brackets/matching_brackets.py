20e4f2632ae082fa5d366be46c5f58b1ea6ad4d4
Sascha Gruner <sascha.gruner@icloud.com>
2024-02-21 18:54:33 +0100

    improvements to matching-brackets-solution

--- matching-brackets/matching_brackets.py
+++ matching-brackets/matching_brackets.py
@@ -1,19 +1,22 @@
 def is_paired(input_string):
+    """
+    checks if open brackets in the input_string have corresponding closing brackets.
+    :input_string: str - string containing the text to be checked
+    :returns: bool - True if brackets match, else False
+    """
+
     OPEN_BRACKETS = ["(", "[", "{"]
     CLOSE_BRACKETS = [")", "]", "}"]
     
-    #needs_break = False
-    #needed_brackets = []
     brackets_stack = []
     for letter in input_string:
         if letter in OPEN_BRACKETS:
             brackets_stack.append(OPEN_BRACKETS.index(letter))
         if letter in CLOSE_BRACKETS:
-            if len(brackets_stack) == 0:
-                return False
-            if letter == CLOSE_BRACKETS[brackets_stack[-1]]:
-                brackets_stack.pop()
-            else:
+            try:
+                if letter != CLOSE_BRACKETS[brackets_stack.pop()]:
+                    return False
+            except:
                 return False
                 
     return (len(brackets_stack) == 0)
